33a601ce4261c807bf264ff1a794f7ea7f4ed796
Rodrigo Lira <rodrigo.lira@trinks.com>
2020-05-02 11:49:01 -0300

    adding the blank project

---
+++ src/Scrambler.Tests/ScramblerTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Scrambler.Tests
+{
+    public class ScramblerTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ src/Scrambler/Scrambler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Scrambler
+{
+    public static class Scrambler
+    {
+        public static string Encode(int number) {
+            return default(string);
+        }
+
+        public static int Decode (string encodedText) {
+            return default(int);
+        }
+    }
+}
